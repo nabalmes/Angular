170b3113841448d63b44a947cff5bbb6fd934288
nabalmes <neilbalmes4200@gmail.com>
2023-05-31 17:11:19 +0800

    used different method of bindings

--- know-angular/src/app/app.component.ts
+++ know-angular/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'know-angular';
+
+  allowNewServer = false
+  serverCreationStatus = "No server created!"
+  serverName = "";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server created successfully! Name is " + this.serverName
+    console.log("button clicked!");
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value
+  }
 }
