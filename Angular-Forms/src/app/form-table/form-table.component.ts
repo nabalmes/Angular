e6c5df46398a0633836c1e71dc18e4b60576dd88
nabalmes <neilbalmes4200@gmail.com>
2024-02-16 20:46:38 +0800

    added new component

---
+++ Angular-Forms/src/app/form-table/form-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-table',
+  templateUrl: './form-table.component.html',
+  styleUrls: ['./form-table.component.css']
+})
+export class FormTableComponent {
+
+}
